212bff5f2c8cc0543f82411e018944bd5e8733e4
Huang-Ming Huang <huangming.huang@gmail.com>
2017-02-22 16:19:00 -0600

    Fix ACE_TAO_WHITELIST_TARGETS

--- cmake/find_or_download_tao.cmake
+++ cmake/find_or_download_tao.cmake
@@ -15,10 +15,16 @@ if (NOT TARGET TAO)
     )
 
     set(DDS_WHITELIST_TARGETS ${WHITELIST_TARGETS})
-    set(ACE_TAO_WHITELIST_TARGETS ACE ace_gperf TAO_IDL_FE TAO_IDL_BE TAO_IDL_EXE
-                          TAO TAO_AnyTypeCode TAO_CodecFactory TAO_BiDirGIOP TAO_CSD_Framework TAO_CSD_ThreadPool
-                          TAO_Codeset TAO_DynamicInterface TAO_ImR_Client TAO_IORManip TAO_IORTable TAO_Messaging
-                          TAO_PI TAO_PortableServer TAO_Valuetype TAO_Svc_Utils)
+    set(ACE_TAO_WHITELIST_TARGETS
+      ACE ACE_XML_Utils ace_gperf
+      TAO_IDL_FE TAO_IDL_BE TAO_IDL_EXE
+      TAO TAO_AnyTypeCode TAO_CodecFactory TAO_BiDirGIOP TAO_CSD_Framework TAO_CSD_ThreadPool
+      TAO_Codeset TAO_DynamicInterface TAO_ImR_Client TAO_IORManip TAO_IORTable TAO_Messaging
+      TAO_PI TAO_PortableServer TAO_Valuetype TAO_Svc_Utils
+      TAO_Async_ImR_Client_IDL TAO_ImR_Activator_IDL TAO_ImR_Locator_IDL TAO_ImR_Activator
+      TAO_ImR_Locator ImR_Locator_Service ImR_Activator_Service tao_imr
+      Naming_Service Hello_Server tao_nsadd
+    )
 
     set(WHITELIST_TARGETS "${ACE_TAO_WHITELIST_TARGETS}" CACHE STRING "" FORCE)
     add_subdirectory(${ACE_TAO_SOURCE_DIR} ${ACE_TAO_BINARY_DIR})
