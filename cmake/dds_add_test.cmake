a212d5e32b6fd7aa6b6453666358a293e983e671
Huang-Ming Huang <huangming.huang@gmail.com>
2017-03-12 20:36:58 -0500

    Avoid FACE and JAVA tests to run in parallel

--- cmake/dds_add_test.cmake
+++ cmake/dds_add_test.cmake
@@ -53,7 +53,7 @@ function(dds_configure_test_files)
 endfunction()
 
 function(dds_add_test name)
-  set(multiValueArgs COMMAND REQUIRES LABELS)
+  set(multiValueArgs COMMAND REQUIRES LABELS RESOURCE_LOCK)
   cmake_parse_arguments(_arg "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
   if (_arg_REQUIRES)
@@ -78,9 +78,9 @@ function(dds_add_test name)
   add_test(NAME "${name}"
            COMMAND ${CMAKE_COMMAND} -E env perl ${_arg_COMMAND}
   )
-
+  list(APPEND _arg_RESOURCE_LOCK "${CMAKE_CURRENT_LIST_FILE}")
   set_tests_properties("${name}" PROPERTIES
     LABELS "${_arg_LABELS}"
-    RESOURCE_LOCK "${CMAKE_CURRENT_LIST_FILE}"
+    RESOURCE_LOCK "${_arg_RESOURCE_LOCK}"
   )
 endfunction()
