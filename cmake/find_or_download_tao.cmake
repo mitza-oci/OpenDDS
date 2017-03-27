bb75a3f3cb847372639f4894913d70a4c54b06cb
Huang-Ming Huang <huangming.huang@gmail.com>
2017-03-27 08:56:36 -0500

    Allows using ACE_TAO_SOURCE_DIR to import ACE_TAO source tree

--- cmake/find_or_download_tao.cmake
+++ cmake/find_or_download_tao.cmake
@@ -1,27 +1,25 @@
 
 if (NOT TARGET TAO)
-  if (TAO_DIR STREQUAL "${CMAKE_CURRENT_BINARY_DIR}/ACE_TAO-build/TAO")
-    set(ACE_TAO_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/ACE_TAO-src)
-    set(ACE_TAO_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/ACE_TAO-build)
+  if (ACE_TAO_SOURCE_DIR)
     include(ACE_TAO_for_DDS.cmake)
+    set(ACE_TAO_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/ACE_TAO-build)
     add_subdirectory(${ACE_TAO_SOURCE_DIR} ${ACE_TAO_BINARY_DIR})
-  else()
-    message("before find_package(TAO) TAO_DIR=${DIR}\n")
+  else(ACE_TAO_SOURCE_DIR)
     find_package(TAO CONFIG)
     if (NOT TAO_FOUND)
       include(cmake/DownloadProject.cmake)
-
       download_project(PROJ                ACE_TAO
                        GIT_REPOSITORY      https://github.com/huangminghuang/ACE_TAO.git
                        GIT_SHALLOW 1
                        GIT_TAG             cmake
                        UPDATE_DISCONNECTED 1
       )
+      set(ACE_TAO_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/ACE_TAO-src CACHE PATH "")
       include(ACE_TAO_for_DDS.cmake)
       add_subdirectory(${ACE_TAO_SOURCE_DIR} ${ACE_TAO_BINARY_DIR})
-    else()
+    else(NOT TAO_FOUND)
       message("-- Found TAO: ${TAO_DIR} (found version \"${TAO_VERSION}\")")
     endif(NOT TAO_FOUND)
-  endif()
-endif()
+  endif(ACE_TAO_SOURCE_DIR)
+endif(NOT TARGET TAO)
 
