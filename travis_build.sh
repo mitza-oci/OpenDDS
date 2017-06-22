f3b2a284125bb43bc181456b9ff8ca45b81abec7
Huang-Ming Huang <huangming.huang@gmail.com>
2017-06-21 20:47:10 -0500

    Refactor ace_try_enable_ccache()

--- travis_build.sh
+++ travis_build.sh
@@ -7,7 +7,7 @@ echo DDS_BUILD_FLAGS=$DDS_BUILD_FLAGS
 # - printf "max_size=450M\nsloppiness=include_file_ctime;include_file_mtime" > ~/.ccache/ccache.conf
 cmake -GNinja $DDS_BUILD_FLAGS .. && ninja -j 6
 cd ..
-tar -czvf build.tar.gz build  --exclude=*.o
+tar -czf build.tar.gz build  --exclude=*.o
 aws s3 cp build.tar.gz s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/build.tar.gz
 
 # - aws s3 sync --quiet s3://com.ociweb.opendds/$TRAVIS_BRANCH/$PIPELINE/.ccache ~/.ccache || true
