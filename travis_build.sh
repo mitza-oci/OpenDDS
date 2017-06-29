1382d7dd5421026af2379db97e05774bfb431850
Huang-Ming Huang <huangming.huang@gmail.com>
2017-06-29 08:11:49 -0500

    Another try for ccache on travis ci

--- travis_build.sh
+++ travis_build.sh
@@ -5,9 +5,16 @@ mkdir build
 cd build
 echo DDS_BUILD_FLAGS=$DDS_BUILD_FLAGS
 # - printf "max_size=450M\nsloppiness=include_file_ctime;include_file_mtime" > ~/.ccache/ccache.conf
+
+if [ "$CCACHE" == "1" ]; then
+    aws s3 sync --quiet s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/.ccache ~/.ccache || true
+fi
+
 cmake -GNinja $DDS_BUILD_FLAGS .. && ninja -j 6
 cd ..
 tar -czf build.tar.gz build  --exclude=*.o
 aws s3 cp build.tar.gz s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/build.tar.gz
 
-# - aws s3 sync --quiet s3://com.ociweb.opendds/$TRAVIS_BRANCH/$PIPELINE/.ccache ~/.ccache || true
+if [ "$CCACHE" == "1" ]; then
+    aws s3 sync --quiet --delete s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/.ccache ~/.ccache || true
+fi
