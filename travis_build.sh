55d9cbf945aeb6ac4ea284f27614969d56045c18
Huang-Ming Huang <huangming.huang@gmail.com>
2017-06-29 19:14:01 -0500

    fix travis script not returning error status

--- travis_build.sh
+++ travis_build.sh
@@ -11,6 +11,8 @@ if [ "$CCACHE" == "1" ]; then
 fi
 
 cmake -GNinja $DDS_BUILD_FLAGS .. && ninja -j 6
+status=$?
+
 cd ..
 tar -czf build.tar.gz build  --exclude=*.o
 aws s3 cp build.tar.gz s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/build.tar.gz
@@ -18,3 +20,5 @@ aws s3 cp build.tar.gz s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/build.
 if [ "$CCACHE" == "1" ]; then
     aws s3 sync --quiet --delete s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/.ccache ~/.ccache || true
 fi
+
+exit $status
--- travis_run_tests.sh
+++ travis_run_tests.sh
@@ -3,6 +3,8 @@ BUILD_NAME=$1
 aws s3 cp s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/build.tar.gz .
 tar zxf build.tar.gz
 cd build
-ctest -j 4
-aws s3 cp Testing/Temporary/LastTest.log s3://com.ociweb.opendds/Logs/${TRAVIS_JOB_NUMBER}/LastTest.log
-echo The log is available from http://com.ociweb.opendds.s3-website-us-east-1.amazonaws.com/Logs/${TRAVIS_JOB_NUMBER}/LastTest.log
+if ! ctest -j 6; then
+    aws s3 cp Testing/Temporary/LastTest.log s3://com.ociweb.opendds/Logs/${TRAVIS_JOB_NUMBER}/LastTest.log
+    echo The log is available from http://com.ociweb.opendds.s3-website-us-east-1.amazonaws.com/Logs/${TRAVIS_JOB_NUMBER}/LastTest.log
+    exit 1
+fi
