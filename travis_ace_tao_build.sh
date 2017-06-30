245ceec63fa663354ee8ae0380fdf522025e97fa
Huang-Ming Huang <huangming.huang@gmail.com>
2017-06-29 21:14:13 -0500

    Remove ccache support for travis

---
+++ travis_ace_tao_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BUILD_NAME=$1
+export CXX=$COMPILER
+mkdir ACE_TAO/build
+cd ACE_TAO/build
+echo BUILD_FLAGS=$BUILD_FLAGS
+
+
+cmake -GNinja $BUILD_FLAGS .. && ninja -j 6 && ninja install
+status=$?
+
+cd ../..
+tar -czf ace_tao.tar.gz ACE_TAO/build  --exclude=*.o
+aws s3 cp ace_tao.tar.gz s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/ace_tao.tar.gz
+
+exit $status
