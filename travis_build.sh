7f33b07b494132d3815cfb85d914e3be26ddbf29
Huang-Ming Huang <huangming.huang@gmail.com>
2017-07-10 06:12:37 -0500

    Fix travis MacOS build

--- travis_build.sh
+++ travis_build.sh
@@ -9,7 +9,7 @@ cmake -GNinja $DDS_BUILD_FLAGS .. && ninja -j $BUILD_JOBS
 status=$?
 
 cd ..
-tar -czf build.tar.gz build  --exclude=*.o
+tar czf build.tar.gz build  --exclude=*.o
 aws s3 cp build.tar.gz s3://com.ociweb.opendds/$TRAVIS_BRANCH/$BUILD_NAME/build.tar.gz
 
 
